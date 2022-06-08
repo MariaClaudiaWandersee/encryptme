e9cd4cd66d34fee6459244322e9a15d2d7f3d1ad
MARIA CLAUDIA DE BRITTO WANDERSEE <maria.wandersee@alunos.sc.senac.br>
2022-06-08 20:48:11 -0300

    Tela categoria completa.

--- Views/AtualizarCategoria.cs
+++ Views/AtualizarCategoria.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Threading;
 using System.IO;  
 using Controllers;
+using Models;
 
 
   public class AtualizarCategoria : Form
@@ -20,10 +21,17 @@ using Controllers;
         TextBox txtDescricao;
         Button btnConfirm;
         Button btnCancel;
-        ListView listView;
+        Categorias parent;
+        int id;
 
-        public AtualizarCategoria()
+        public AtualizarCategoria(Categorias parent)
         {
+            this.parent = parent;
+            ListViewItem item = this.parent.listView.SelectedItems[0];
+            int id = Convert.ToInt32(item.Text);
+            this.id = id;
+            Categoria categoria = CategoriaController.GetCategoria(id);
+
             this.lblNome = new Label();
             this.lblNome.Text = "Nome";
             this.lblNome.Location = new Point(10, 20);
@@ -35,10 +43,12 @@ using Controllers;
             this.txtNome = new TextBox();
             this.txtNome.Location = new Point(10, 50);
             this.txtNome.Size = new Size(280, 30);
+            this.txtNome.Text = categoria.Nome;
 
             this.txtDescricao = new TextBox();
             this.txtDescricao.Location = new Point(10, 135);
             this.txtDescricao.Size = new Size(280, 30);
+            this.txtDescricao.Text = categoria.Descricao;
 
             this.btnConfirm = new Button();
             this.btnConfirm.Text = "Confirmar";
@@ -58,9 +68,8 @@ using Controllers;
             this.Controls.Add(this.txtNome);
             this.Controls.Add(this.txtDescricao);
 
-
-            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.btnCancel);
 
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -70,7 +79,17 @@ using Controllers;
         }
         private void handleConfirmClick(object sender, EventArgs e)
         {
-           ListViewItem selectedItem = listView.SelectedItems[0];
+           try 
+            {
+                CategoriaController.AlterarCategoria(this.id, this.txtNome.Text, this.txtDescricao.Text);
+                MessageBox.Show("Categoria cadastrada com sucesso!", "Sucesso", MessageBoxButtons.OK);
+                this.parent.loadList();
+                this.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
         }
         private void handleCancelClick(object sender, EventArgs e)
         {
